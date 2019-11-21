ffbecd68114fbe3459cfc99e7a8401cdb6113b27
HS <gustjr0413@kaist.ac.kr>
2019-11-21 19:06:36 +0900

    create Rmatrix

---
+++ Rmatrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Rmatrix():
+    def __init__(self, action_dim):
+        self.action_dim = action_dim
+        self.M = np.zeros(action_dim)
+
+    def update(self, action, reward):
+        self.M[action] = reward
+
+    def select_action(self):
+        return np.argmax(self.M)
+
+    def reset(self):
+        self.M = np.zeros(action_dim)
