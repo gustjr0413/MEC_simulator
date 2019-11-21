9d76ba09bbe9dfbb31d04c6ff6dcdf205bd30acc
HS <gustjr0413@kaist.ac.kr>
2019-11-21 19:52:04 +0900

    create NN_train

---
+++ NN_train.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, hidden_size2,hidden_size3, hidden_size4, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        # self.bn1 = nn.BatchNorm1d(hidden_size)
+        # self.drop1 = nn.Dropout(0.1)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size2)
+        # self.bn2 = nn.BatchNorm1d(hidden_size2)
+        # self.drop2 = nn.Dropout(0.1)
+        self.fc3 = nn.Linear(hidden_size2, hidden_size3)
+        # self.bn3 = nn.BatchNorm1d(hidden_size3)
+        # self.drop3 = nn.Dropout(0.1)
+        self.fc4 = nn.Linear(hidden_size3, num_classes)
+
+    def forward(self, x):
+        x = self.relu((self.fc1(x)))
+        x = self.relu((self.fc2(x)))
+        x = self.relu((self.fc3(x)))
+        out = self.fc4(x)
+        return out
