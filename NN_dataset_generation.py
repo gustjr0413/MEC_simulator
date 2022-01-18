d12dfc0782e8945bb440bb53db4b28acf5b5c0d9
gustjr0413 <47532250+gustjr0413@users.noreply.github.com>
2022-01-18 15:40:24 +0900

    Update NN_dataset_generation.py

--- NN_dataset_generation.py
+++ NN_dataset_generation.py
@@ -1,28 +1,21 @@
-import numpy as np
 import pandas as pd
-import Environment_search as Env
-import transform as tr
+from transform import transform
+from Environment_search import Environment_search
 
 
 num_user = 1000000
 C_total = 110e9
-R_n = 43.9e6
-R_C = 150.6e6
+R_n = 41.2e6 # uplink data rate
+R_C = 360.3e6 # downlink data rate
 state_dim = 6
 action_dim = 300*200
 
 
 
-env = Environment_search(num_user, C_total, R_n, R_C, state_dim, action_dim)
-state_dim = env.state_dim
-action_dim = env.action_dim
-state_action_list = []
-
-user_set = env.generate_user()
 for user in range(num_user):
     state, t = env.get_state()
     action, max_payment = env.search(state)
-    if (user+1)%50==0:
+    if (user+1)%100==0:
         print(" user {} update is finished".format(user+1))
         print(" action {}, payment {}".format(transform().to2dim(action), max_payment))
     temp_file = state.tolist()
